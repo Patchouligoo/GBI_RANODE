5500cc6a2a1472ec743a3f64a67e5e8e96d98a17
“RunzeLi” <runze.li@yale.edu>
2025-02-24 07:28:19 -0800

    [dev] now use the lower bound of fitting to define 95 CI

--- src/fitting/fitting.py
+++ src/fitting/fitting.py
@@ -5,11 +5,9 @@ from matplotlib.backends.backend_pdf import PdfPages
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, WhiteKernel, ConstantKernel as C
 
-def fit_likelihood(x_values, y_values, w_true, events_num, output_path):
+def fit_likelihood(x_values, y_values_mean, y_values_std, w_true, events_num, output_path):
 
     x_values = x_values.reshape(-1, 1)
-    y_values_mean = np.mean(y_values, axis=-1)
-    y_values_std = np.std(y_values, axis=-1)
 
     # define the kernel
     kernel = C(1.0, (1e-3, 1e3)) * RBF(1.0, (1e-2, 1e2)) #+ WhiteKernel(noise_level=0.01, noise_level_bounds=(1e-10, 1e+1))
@@ -25,12 +23,25 @@ def fit_likelihood(x_values, y_values, w_true, events_num, output_path):
     y_pred = y_pred.flatten()
     sigma = sigma.flatten()
 
+    y_lower_bound = y_pred - 1.96 * sigma
+    y_upper_bound = y_pred + 1.96 * sigma
+
     # find peak of the likelihood
     arg_max_likelihood = np.argmax(y_pred)
     max_likelihood = y_pred[arg_max_likelihood]
     mu_pred = np.power(10, x_pred[arg_max_likelihood])
     mu_true = np.power(10, w_true)
-    CI_95_likelihood = max_likelihood - np.log(2)/events_num
+
+    # 95% CI of the peak
+    arg_max_y_lower_bound = np.argmax(y_lower_bound)
+    arg_max_y_upper_bound = np.argmax(y_upper_bound)
+    max_y_lower_bound = y_lower_bound[arg_max_y_lower_bound]
+    CI_95_likelihood_lower = max_y_lower_bound - np.log(2)/events_num
+    # find intersection of lower CI with y upper bound as range of mu values
+    mu_lowerbound_index = np.argmin(np.abs(y_upper_bound[0:arg_max_y_upper_bound] - CI_95_likelihood_lower))
+    mu_lowerbound = np.power(10, x_pred[0:arg_max_y_upper_bound][mu_lowerbound_index])
+    mu_upperbound_index = np.argmin(np.abs(y_upper_bound[arg_max_y_upper_bound:] - CI_95_likelihood_lower))
+    mu_upperbound = np.power(10, x_pred[arg_max_y_upper_bound:][mu_upperbound_index])
 
     with PdfPages(output_path) as pdf:
         f = plt.figure(figsize=(10, 8))
@@ -39,11 +50,10 @@ def fit_likelihood(x_values, y_values, w_true, events_num, output_path):
         plt.plot(x_pred, y_pred, label='fit func', color='red')
         plt.fill_between(x_pred, y_pred - 1.96 * sigma, y_pred + 1.96 * sigma, alpha=0.2, color='red')
 
-        # plot 95% CI of the peak
-        
-        plt.axhline(y=CI_95_likelihood, color='red', linestyle='--', label='95% CI of $\mu$')
-
+        # plot 95% CI and the peak
         plt.scatter([x_pred[arg_max_likelihood]], [max_likelihood], color='red', label=f'peak $\mu$ {mu_pred:.4f}')  # peak w value
+        plt.axhline(y=CI_95_likelihood_lower, color='blue', linestyle='--')
+        plt.scatter([np.log10(mu_lowerbound), np.log10(mu_upperbound)], [CI_95_likelihood_lower, CI_95_likelihood_lower], color='blue', label=f'95% CI of $\mu$ [{mu_lowerbound:.4f}, {mu_upperbound:.4f}]')
         
         # true w value
         plt.axvline(x=w_true, color='black', linestyle='--', label=f'true $\mu$ {mu_true:.4f}')
