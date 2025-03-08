ca1e90d0cca07329b1e273ceaa1e976cec1b1db7
“RunzeLi” <runze.li@yale.edu>
2025-03-07 19:17:04 -0800

    [format] correct format of code

--- src/fitting/fitting.py
+++ src/fitting/fitting.py
@@ -9,13 +9,26 @@ from sklearn.gaussian_process.kernels import RBF, WhiteKernel, ConstantKernel as
 from src.utils.utils import NumpyEncoder
 from src.utils.utils import find_zero_crossings
 
-def fit_likelihood(x_values, y_values_mean, y_values_std, w_true, events_num, output_path, logbased=True):
+
+def fit_likelihood(
+    x_values,
+    y_values_mean,
+    y_values_std,
+    w_true,
+    events_num,
+    output_path,
+    logbased=True,
+):
 
     x_values = x_values.reshape(-1, 1)
 
     # define the kernel
-    kernel = C(1.0, (1e-3, 1e3)) * RBF(1e-3, (1e-5, 1e2)) #+ WhiteKernel(noise_level=0.01, noise_level_bounds=(1e-10, 1e+1))
-    gp = GaussianProcessRegressor(kernel=kernel, alpha=y_values_std**2, n_restarts_optimizer=100)
+    kernel = C(1.0, (1e-3, 1e3)) * RBF(
+        1e-3, (1e-5, 1e2)
+    )  # + WhiteKernel(noise_level=0.01, noise_level_bounds=(1e-10, 1e+1))
+    gp = GaussianProcessRegressor(
+        kernel=kernel, alpha=y_values_std**2, n_restarts_optimizer=100
+    )
     gp.fit(x_values, y_values_mean)
 
     # --- Make Predictions on a Fine Grid ---
@@ -46,31 +59,44 @@ def fit_likelihood(x_values, y_values_mean, y_values_std, w_true, events_num, ou
     # ------------------------------- 95% CI of max likelihood -------------------------------
     # given x_pred, y_pred, and 95CI likelihood drop = np.log(2)/events_num, find the first left
     # intersection of max likelihood - drop with y_pred as the lower bound of the 95% CI
-    CI_95_likelihood = max_likelihood - np.log(2)/events_num
+    CI_95_likelihood = max_likelihood - np.log(2) / events_num
 
     # ---------------------------------------------------------------------------------------
 
     with PdfPages(output_path) as pdf:
         f = plt.figure(figsize=(10, 8))
-        plt.scatter(x_values.flatten(), y_values_mean, label='test points', color='black')
-        plt.errorbar(x_values.flatten(), y_values_mean, yerr=y_values_std, fmt='o', color='black')
-        plt.plot(x_pred, y_pred, label='fit func', color='red')
-        plt.fill_between(x_pred, y_pred - 1.96 * sigma, y_pred + 1.96 * sigma, alpha=0.2, color='red')
+        plt.scatter(
+            x_values.flatten(), y_values_mean, label="test points", color="black"
+        )
+        plt.errorbar(
+            x_values.flatten(), y_values_mean, yerr=y_values_std, fmt="o", color="black"
+        )
+        plt.plot(x_pred, y_pred, label="fit func", color="red")
+        plt.fill_between(
+            x_pred, y_pred - 1.96 * sigma, y_pred + 1.96 * sigma, alpha=0.2, color="red"
+        )
 
         # plot 95% CI and the peak
-        plt.scatter([x_pred[arg_max_likelihood]], [max_likelihood], color='red', label=f'peak $\mu$ {mu_pred:.4f}')  # peak w value
-        plt.axhline(y=CI_95_likelihood, color='blue', linestyle='--')
+        plt.scatter(
+            [x_pred[arg_max_likelihood]],
+            [max_likelihood],
+            color="red",
+            label=f"peak $\mu$ {mu_pred:.4f}",
+        )  # peak w value
+        plt.axhline(y=CI_95_likelihood, color="blue", linestyle="--")
         # true w value
-        plt.axvline(x=w_true, color='black', linestyle='--', label=f'true $\mu$ {mu_true:.4f}')
+        plt.axvline(
+            x=w_true, color="black", linestyle="--", label=f"true $\mu$ {mu_true:.4f}"
+        )
 
-        plt.title(f'Likelihood fit at true $\mu$ {mu_true:.4f}')
+        plt.title(f"Likelihood fit at true $\mu$ {mu_true:.4f}")
 
         if logbased:
-            plt.xlabel('$log_{10}(\mu)$')
+            plt.xlabel("$log_{10}(\mu)$")
         else:
-            plt.xlabel('$\mu$')
+            plt.xlabel("$\mu$")
 
-        plt.ylabel('likelihood')
+        plt.ylabel("likelihood")
 
         plt.legend()
         pdf.savefig(f)
@@ -84,7 +110,7 @@ def fit_likelihood(x_values, y_values_mean, y_values_std, w_true, events_num, ou
         "y_pred": y_pred,
         "sigma": sigma,
         "CI_95_likelihood": CI_95_likelihood,
-        "CI_95_likelihood_drop": np.log(2)/events_num,
+        "CI_95_likelihood_drop": np.log(2) / events_num,
         "x_raw": x_values.flatten(),
         "y_raw": y_values_mean,
         "y_raw_std": y_values_std,
@@ -93,7 +119,15 @@ def fit_likelihood(x_values, y_values_mean, y_values_std, w_true, events_num, ou
     return output_metadata
 
 
-def bootstrap_and_fit(prob_S_list, prob_B_list, mu_scan_values, mu_true, output_dir, bootstrap_num=100, random_seed=42):
+def bootstrap_and_fit(
+    prob_S_list,
+    prob_B_list,
+    mu_scan_values,
+    mu_true,
+    output_dir,
+    bootstrap_num=100,
+    random_seed=42,
+):
 
     # prob_S_list has shape (num_scan_points, num_models, num_events)
     # prob_B_list has shape (num_scan_points, num_models, num_events)
@@ -103,14 +137,14 @@ def bootstrap_and_fit(prob_S_list, prob_B_list, mu_scan_values, mu_true, output_
     # mu_scan_values = mu_scan_values[5:]
     # prob_B_list = prob_B_list[5:]
     # prob_S_list = prob_S_list[5:]
-    
+
     # compute the nominal likelihood
-    prob_S_nominal = prob_S_list.mean(axis=1) # shape is (num_scan_points, num_events)
-    prob_B_nominal = prob_B_list.mean(axis=1) # shape is (num_scan_points, num_events)
+    prob_S_nominal = prob_S_list.mean(axis=1)  # shape is (num_scan_points, num_events)
+    prob_B_nominal = prob_B_list.mean(axis=1)  # shape is (num_scan_points, num_events)
     # likelihood_nominal = mu_scan_values.reshape(-1, 1) * prob_S_nominal + (1 - mu_scan_values.reshape(-1, 1)) * prob_B_nominal
     # log_likelihood_nominal = np.log(likelihood_nominal)
     # log_likelihood_nominal_mean = log_likelihood_nominal.mean(axis=1)
-    
+
     # Now bootstrap classifiers in model_S to get the uncertainty in the likelihood
     np.random.seed(random_seed)
     bootstrap_model_S_index = np.random.choice(
@@ -126,7 +160,10 @@ def bootstrap_and_fit(prob_S_list, prob_B_list, mu_scan_values, mu_true, output_
         # bootstrap_model_B_index_i = bootstrap_model_B_index[index]
         # prob_B_bootstrap_i = prob_B_list[:, bootstrap_model_B_index_i].mean(axis=1)
 
-        likelihood_bootstrap_i = mu_scan_values.reshape(-1, 1) * prob_S_bootstrap_i + (1 - mu_scan_values.reshape(-1, 1)) * prob_B_nominal
+        likelihood_bootstrap_i = (
+            mu_scan_values.reshape(-1, 1) * prob_S_bootstrap_i
+            + (1 - mu_scan_values.reshape(-1, 1)) * prob_B_nominal
+        )
         log_likelihood_bootstrap_i = np.log(likelihood_bootstrap_i)
         log_likelihood_bootstrap_i_mean = log_likelihood_bootstrap_i.mean(axis=1)
 
@@ -144,8 +181,12 @@ def bootstrap_and_fit(prob_S_list, prob_B_list, mu_scan_values, mu_true, output_
     x_values = x_values.reshape(-1, 1)
 
     # define the kernel
-    kernel = C(1.0, (1e-3, 1e3)) * RBF(1e-3, (1e-5, 1e2)) #+ WhiteKernel(noise_level=0.01, noise_level_bounds=(1e-10, 1e+1))
-    gp = GaussianProcessRegressor(kernel=kernel, alpha=log_likelihood_nominal_std**2, n_restarts_optimizer=100)
+    kernel = C(1.0, (1e-3, 1e3)) * RBF(
+        1e-3, (1e-5, 1e2)
+    )  # + WhiteKernel(noise_level=0.01, noise_level_bounds=(1e-10, 1e+1))
+    gp = GaussianProcessRegressor(
+        kernel=kernel, alpha=log_likelihood_nominal_std**2, n_restarts_optimizer=100
+    )
     gp.fit(x_values, y_values)
 
     # --- Make Predictions on a Fine Grid ---
@@ -161,13 +202,13 @@ def bootstrap_and_fit(prob_S_list, prob_B_list, mu_scan_values, mu_true, output_
     max_likelihood_index = np.argmax(y_pred)
     max_likelihood_w = np.power(10, x_pred[max_likelihood_index])
     max_likelihood_w_log = x_pred[max_likelihood_index]
-    
+
     # ------------------------------- 95% CI of max likelihood -------------------------------
-    CI95_likelihood = max_likelihood - np.log(2)/event_num
+    CI95_likelihood = max_likelihood - np.log(2) / event_num
     diff = y_pred_upper_bound - CI95_likelihood
     crossings = find_zero_crossings(x_pred, diff)
     # Separate them into those on the left vs. right of the maximum
-    left_crossings  = [c for c in crossings if c < max_likelihood_w_log]
+    left_crossings = [c for c in crossings if c < max_likelihood_w_log]
     right_crossings = [c for c in crossings if c > max_likelihood_w_log]
     # If there is more than one intersection on each side, we take the
     # minimum on the left and the maximum on the right to be conservative
@@ -180,23 +221,46 @@ def bootstrap_and_fit(prob_S_list, prob_B_list, mu_scan_values, mu_true, output_
     # make plots
     f = plt.figure()
 
-    plt.scatter(x_values.flatten(), y_values, label='test points', color='black')
-    plt.errorbar(x_values.flatten(), y_values, yerr=log_likelihood_nominal_std, fmt='o', color='black')
+    plt.scatter(x_values.flatten(), y_values, label="test points", color="black")
+    plt.errorbar(
+        x_values.flatten(),
+        y_values,
+        yerr=log_likelihood_nominal_std,
+        fmt="o",
+        color="black",
+    )
 
-    plt.plot(x_pred, y_pred, label='fit func', color='red')
-    plt.fill_between(x_pred, y_pred_lower_bound, y_pred_upper_bound, alpha=0.2, color='red')
+    plt.plot(x_pred, y_pred, label="fit func", color="red")
+    plt.fill_between(
+        x_pred, y_pred_lower_bound, y_pred_upper_bound, alpha=0.2, color="red"
+    )
 
     # plot 95% CI and the peak
-    plt.scatter([max_likelihood_w_log], [max_likelihood], color='red', label=f'peak $\mu$ {max_likelihood_w:.4f}')  # peak w value
-    plt.axhline(y=CI95_likelihood, color='blue', linestyle='--', label=f'95% CI, [{mu_left:.4f}, {mu_right:.4f}]')
+    plt.scatter(
+        [max_likelihood_w_log],
+        [max_likelihood],
+        color="red",
+        label=f"peak $\mu$ {max_likelihood_w:.4f}",
+    )  # peak w value
+    plt.axhline(
+        y=CI95_likelihood,
+        color="blue",
+        linestyle="--",
+        label=f"95% CI, [{mu_left:.4f}, {mu_right:.4f}]",
+    )
 
-    plt.axvline(x=np.log10(mu_true), color='black', linestyle='--', label=f'true $\mu$ {mu_true:.4f}')
+    plt.axvline(
+        x=np.log10(mu_true),
+        color="black",
+        linestyle="--",
+        label=f"true $\mu$ {mu_true:.4f}",
+    )
 
-    plt.title(f'Likelihood fit at true $\mu$ {mu_true:.4f}')
+    plt.title(f"Likelihood fit at true $\mu$ {mu_true:.4f}")
 
-    plt.xlabel('$log_{10}(\mu)$')
+    plt.xlabel("$log_{10}(\mu)$")
 
-    plt.ylabel('likelihood')
+    plt.ylabel("likelihood")
 
     plt.legend()
     plt.savefig(output_dir)
